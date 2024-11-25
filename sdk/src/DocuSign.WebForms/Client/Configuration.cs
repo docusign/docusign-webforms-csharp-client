24ccd9abde207a6815c4ea60ff514dcd321dea15
Mudit Garg <35302867+garg-mudit@users.noreply.github.com>
2024-11-25 11:57:06 +0530

    Version 3.0.2-v1.1.0-1.0.4 release (#19)
    Co-authored-by: root <root@devcenteradmin.docusigntest.com>

--- sdk/src/DocuSign.WebForms/Client/Configuration.cs
+++ sdk/src/DocuSign.WebForms/Client/Configuration.cs
@@ -26,7 +26,7 @@ namespace DocuSign.WebForms.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "3.0.1";
+        public const string Version = "3.0.2";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
